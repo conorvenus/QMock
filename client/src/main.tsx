6152397ce80b6af826977159d3d66dd144606dde
conorv <70652079+conorvenus@users.noreply.github.com>
2023-09-19 16:07:01 +0100

    Transformed the App component into a private route

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,20 +1,26 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import { AuthProvider } from 'react-auth-kit'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import { AuthProvider, useIsAuthenticated } from 'react-auth-kit'
 import Navbar from './components/Navbar.tsx'
 import NotFound from './routes/NotFound.tsx'
 import Register from './routes/Register.tsx'
 import Login from './routes/Login.tsx'
 import Logout from './routes/Logout.tsx'
 
+const PrivateRoute = ({ Component } : { Component: React.FC }) => {
+    const isAuthenticated = useIsAuthenticated();
+    const auth = isAuthenticated();
+    return auth ? <Component /> : <Navigate to="/login" />;
+};
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <AuthProvider authType="cookie" authName="auth">
         <BrowserRouter>
             <Navbar />
             <Routes>
-                <Route path="/" element={<App />} />
+                <Route path="/" element={<PrivateRoute Component={App} />} />
                 <Route path="/register" element={<Register />} />
                 <Route path="/login" element={<Login />} />
                 <Route path="/logout" element={<Logout />} />
